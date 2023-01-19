dabb22162536b4b926fd59d5932e7b54e28619c6
KSMubasshir <ksmubasshir@mail.com>
2023-01-19 09:26:53 -0500

    DoLTEst implementation of srsRAN used

--- bin/common.sh
+++ bin/common.sh
@@ -1,4 +1,5 @@
 
 SRCDIR=/var/tmp
 CFGDIR=/local/repository/etc
-SRS_REPO="https://github.com/srsran/srsRAN"
+#SRS_REPO="https://github.com/srsran/srsRAN"
+SRS_REPO="https://github.com/SysSec-KAIST/DoLTEst"
