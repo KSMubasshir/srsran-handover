58f62fc87052346725eaca40faa10268156c2f0b
KSMubasshir <ksmubasshir@mail.com>
2023-01-28 19:39:20 -0500

    Using fbs-srsRAN implementation of srsRAN

--- bin/common.sh
+++ bin/common.sh
@@ -2,4 +2,5 @@
 SRCDIR=/var/tmp
 CFGDIR=/local/repository/etc
 #SRS_REPO="https://github.com/srsran/srsRAN"
-SRS_REPO="https://github.com/SysSec-KAIST/DoLTEst"
+#SRS_REPO="https://github.com/SysSec-KAIST/DoLTEst"
+SRS_REPO="https://github.com/KSMubasshir/fbs-SRSRAN.git"
